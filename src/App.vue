efc09dff8b73939f101357c0321ae5ac644f8077
mfts <18267094443@163.com>
2023-06-17 16:39:24 +0800

    允许切换视频源，添加显式的信息吐出

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <script lang="ts" setup>
 import axios from 'axios'
 
-const url = ref('webrtc://101.34.3.87/live/1686638972318')
+const failUrl = ref('webrtc://101.34.3.87/live/1686990987248')
 const webrtcRef = ref<HTMLVideoElement | null>(null)
+const message = ref<string[]>([])
 
 let rtcPeerConnection: RTCPeerConnection
 let offer: RTCSessionDescriptionInit
 
 onMounted(async () => {
+  webrtcRef.value?.addEventListener('play', () => {
+    message.value.push('视频已被触发播放')
+  })
+  webrtcRef.value?.addEventListener('loadedmetadata', () => {
+    message.value.push('已加载元数据。')
+  })
+  webrtcRef.value?.addEventListener('loadeddata', () => {
+    message.value.push('media 中的首帧已经完成加载。')
+  })
+  webrtcRef.value?.addEventListener('playing', () => {
+    message.value.push('由于缺乏数据而暂停或延迟后，播放准备开始。')
+  })
+})
+
+onUnmounted(() => {
+  disconnect()
+})
+
+async function disconnect() {
+  message.value.push('连接断开')
+  webrtcRef.value?.pause()
+  rtcPeerConnection?.close()
+}
+
+let playPromise: Promise<any>
+
+async function connect(streamurl: string) {
+  if (!webrtcRef.value)
+    return
+
+  webrtcRef.value.load()
+
   rtcPeerConnection = new RTCPeerConnection(undefined)
 
   rtcPeerConnection.ontrack = function (event) {
@@ -24,28 +57,20 @@ onMounted(async () => {
 
   offer = await rtcPeerConnection.createOffer()
   await rtcPeerConnection.setLocalDescription(offer)
-})
 
-onUnmounted(() => {
-  disconnect()
-})
-
-async function disconnect() {
-  webrtcRef.value?.pause()
-  rtcPeerConnection.close()
-}
-
-async function connect() {
+  message.value.push('正在准备连接...')
   const api = 'http://101.34.3.87:1985/rtc/v1/play/'
 
   const clientip = null
 
   const res = await axios.post(api, {
     api,
-    streamurl: url.value,
+    streamurl,
     clientip,
     sdp: offer.sdp,
   })
+
+  // rtcPeerConnection.Remo/
 
   rtcPeerConnection.setRemoteDescription(new RTCSessionDescription({
     type: 'answer',
@@ -53,8 +78,21 @@ async function connect() {
   }))
 
   rtcPeerConnection.addEventListener('connectionstatechange', () => {
-    if (rtcPeerConnection.connectionState === 'connected')
-      webrtcRef.value?.play()
+    if (!webrtcRef.value)
+      return
+
+    if (rtcPeerConnection.connectionState === 'connected') {
+      message.value.push('webrtc 连接成功，正在缓冲加载中')
+
+      webrtcRef.value?.load()
+      playPromise = webrtcRef.value.play()
+      playPromise.then(() => {
+        message.value.push('视频加载成功')
+      }).catch((err) => {
+        message.value.push('视频加载过程中被中断')
+        console.error(err)
+      })
+    }
   })
 }
 </script>
@@ -62,7 +100,7 @@ async function connect() {
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <div>
-      <TheInput v-model="url" />
+      <TheInput v-model="failUrl" />
       <button
         p="x-4 y-2"
         text="center"
@@ -70,7 +108,7 @@ async function connect() {
         bg="transparent"
         border="~ rounded gray-200 dark:gray-700"
         hover:bg-gray
-        @click="connect"
+        @click="connect(failUrl)"
       >
         Connect
       </button>
@@ -85,9 +123,14 @@ async function connect() {
         Disconnect
       </button>
     </div>
+    <ol mt-5>
+      <li v-for="(el, index) in message" :key="index" text-2>
+        <span>{{ index + 1 }}</span> = {{ el }}
+      </li>
+    </ol>
     <div>
       <div ma mt10 h-70 w-120 bg-black>
-        <video ref="webrtcRef" controls ma h-70 />
+        <video ref="webrtcRef" controls ma h-full w-full />
       </div>
     </div>
     <TheFooter />
